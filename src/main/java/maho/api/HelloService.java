44a662edc951b7c4c88a522e3c278fffd456e21a
mariaholmberg <H0udini>
2017-12-31 17:32:53 +0100

    Working restservice

---
+++ src/main/java/maho/api/HelloService.java
@@ -0,0 +1,21 @@
+package maho.api;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+@Path("hello")
+@Produces(MediaType.TEXT_PLAIN)
+public class HelloService  {
+
+
+    public HelloService() {}
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hej världen!";
+    }
+}
